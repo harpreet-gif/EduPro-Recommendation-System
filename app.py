8d8d8261bb5a956a2d36cbaa48b471008f67b76d
harpreet-gif <harpreetkaurrr053@gmail.com>
2026-05-20 14:56:41 +0530

    Initial project upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+
+# Load data
+learner_profile = pd.read_csv("/Users/harpreet/OneDrive/Desktop/EduPro_Project/outputs/clustered_learners.csv")
+courses = pd.read_csv("/Users/harpreet/Desktop/EduPro_Project/data/EduPro Online Platform.xlsx - Courses.csv")
+transactions = pd.read_csv("/Users/harpreet/Desktop/EduPro_Project/data/EduPro Online Platform.xlsx - Transactions.csv")
+
+# Merge data
+data = transactions.merge(courses, on='CourseID', how='left')
+
+# Recommendation Function
+def recommend_courses(user_id, top_n=5):
+    
+    user_cluster = learner_profile[
+        learner_profile['UserID'] == user_id
+    ]['cluster'].values[0]
+    
+    similar_users = learner_profile[
+        learner_profile['cluster'] == user_cluster
+    ]['UserID']
+    
+    cluster_data = data[data['UserID'].isin(similar_users)]
+    
+    user_courses = data[data['UserID'] == user_id]['CourseID']
+    
+    cluster_data = cluster_data[
+        ~cluster_data['CourseID'].isin(user_courses)
+    ]
+    
+    course_scores = cluster_data.groupby('CourseID').agg({
+        'UserID': 'count',
+        'CourseRating': 'mean'
+    }).rename(columns={
+        'UserID': 'popularity',
+        'CourseRating': 'avg_rating'
+    })
+    
+    course_scores['score'] = (
+        course_scores['popularity'] * 0.6 +
+        course_scores['avg_rating'] * 0.4
+    )
+    
+    top_courses = course_scores.sort_values(
+        by='score', ascending=False
+    ).head(top_n)
+    
+    return courses[courses['CourseID'].isin(top_courses.index)]
+
+
+# ---------------- UI ---------------- #
+
+st.title("🎓 EduPro: Personalized Course Recommendation System")
+
+# Select User
+user_id = st.selectbox(
+    "Select User ID",
+    learner_profile['UserID']
+)
+
+# Show User Info
+user_data = learner_profile[
+    learner_profile['UserID'] == user_id
+]
+
+st.subheader("👤 Learner Profile")
+st.write(user_data)
+
+# Show Cluster
+cluster = user_data['cluster'].values[0]
+st.subheader(f"📊 Assigned Segment: {cluster}")
+
+# Recommendation Button
+if st.button("🚀 Get Recommendations"):
+    
+    recs = recommend_courses(user_id)
+    
+    st.subheader("📚 Recommended Courses")
+    st.dataframe(recs)
